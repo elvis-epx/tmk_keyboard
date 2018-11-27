fb1820c93b18d3eaa9cb1ace0f3e59c698a251ed
Elvis Pfützenreuter <epxx@epxx.co>
2018-11-27 17:38:22 -0200

    Changed flags, fixed prototype

--- converter/ps2_usb/led_failure.h
+++ converter/ps2_usb/led_failure.h
@@ -15,4 +15,4 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-void led_signal_failure();
+void led_signal_failure(void);
